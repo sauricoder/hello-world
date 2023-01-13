b537dc21b30efceb9e21c8ef29bd4fe8355d6606
sauricoder <122543575+sauricoder@users.noreply.github.com>
2023-01-13 09:54:39 -0500

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,8 +1,49 @@
 input.onButtonPressed(Button.A, function () {
-    basic.showIcon(IconNames.Happy)
+    basic.showIcon(IconNames.Asleep)
+    basic.showString("HEY THERE HANDSOME")
+})
+input.onGesture(Gesture.TiltLeft, function () {
+    basic.showLeds(`
+        . . . . .
+        . . . . .
+        . . . . #
+        . . . . .
+        . . . . .
+        `)
+    basic.showLeds(`
+        . . . . .
+        . . . . .
+        . . . # .
+        . . . . .
+        . . . . .
+        `)
+    basic.showLeds(`
+        . . . . .
+        . . . . .
+        . . # . .
+        . . . . .
+        . . . . .
+        `)
+    basic.showLeds(`
+        . . . . .
+        . . . . .
+        . # . . .
+        . . . . .
+        . . . . .
+        `)
+    basic.showLeds(`
+        . . . . .
+        . . . . .
+        # . . . .
+        . . . . .
+        . . . . .
+        `)
 })
 input.onButtonPressed(Button.B, function () {
-    basic.showIcon(IconNames.Sad)
+    basic.showIcon(IconNames.Meh)
+    basic.showString("GOOOOFY AHHHHHH")
 })
 basic.showIcon(IconNames.Yes)
-basic.showNumber(0)
+basic.showString("WELCOME TO OHIO")
+basic.showArrow(ArrowNames.East)
+music.playMelody("- - - - - - - - ", 120)
